035ac90fb30e993133c3e61d52a86d137ed6ecb7
TripleJah <orangehedge@protonmail.com>
2023-06-07 16:05:50 -0600

    basic bootstrap layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <div>
+      <Header />
+
+      <main className="py-5">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
