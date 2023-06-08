92295226fd9628907f0ed0784be7ad4ed9223406
TripleJah <orangehedge@protonmail.com>
2023-06-08 17:03:38 -0600

    basic backend and redux implementation

---
+++ frontend/src/store.js
@@ -0,0 +1,21 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { productListReducer } from './reducers/productReducers'
+
+
+
+const reducer = combineReducers({
+    productList: productListReducer
+})
+
+export const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore({
+    reducer: reducer,
+    preloadedState: initialState,
+    middleware: middleware
+});
+
+export default store
