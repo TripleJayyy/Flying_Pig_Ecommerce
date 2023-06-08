33b6dbcc034ae0d8287f6d2cad92933e701304fd
TripleJah <orangehedge@protonmail.com>
2023-06-08 13:42:01 -0600

    api call

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card} from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+
 import { Button } from 'react-bootstrap'
+import axios from "axios";
 
 function ProductScreen() {
-  let { id } = useParams();
 
-  const product = products.find((p) => p._id == id);
+  const productId = useParams();
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${productId.id}`)
+      setProduct(data)
+    }
+
+    fetchProduct()
+
+  }, []);
+ 
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
