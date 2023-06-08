2e9d38b072f6b0420cd698a6718178b559253e42
TripleJah <orangehedge@protonmail.com>
2023-06-07 22:44:12 -0600

    routing for products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import React from "react";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-
-      <main className="py-5">
+      <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            {" "}
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
